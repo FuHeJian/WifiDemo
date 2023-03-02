c155cae75e51eb78bbf8b0ad950f2d6b1dd1e133
DESKTOP-2SQ4923\111 <fuhejian@snoppa.com>
2023-03-02 11:07:00 +0800

    重新推送

--- app/src/main/java/com/example/wifidemo1/adapter/BaseDataBindingAdapter.java
+++ app/src/main/java/com/example/wifidemo1/adapter/BaseDataBindingAdapter.java
@@ -14,8 +14,7 @@ import androidx.recyclerview.widget.ListAdapter;
 import androidx.recyclerview.widget.RecyclerView;
 
 public abstract class BaseDataBindingAdapter<T> extends ListAdapter<T,RecyclerView.ViewHolder> {
-
-    ///ssssssssssssssss
+    
     protected BaseDataBindingAdapter(@NonNull DiffUtil.ItemCallback diffCallback) {
         super(diffCallback);
     }
